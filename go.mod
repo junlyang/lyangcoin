923e695953886d3f93ecbbf1084c64d43f43b69d
junlyang <eomlyang@naver.com>
2021-06-25 01:40:53 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/junlyang/gocoin
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/junlyang/gocoin/blockchain"
+)
+
+func main() {
+	fmt.Println("작동 잘함 ")
+	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data : %s\n", block.Data)
+		fmt.Printf("Hash : %s\n", block.Hash)
+		fmt.Printf("PrevHash : %s\n", block.PrevHash)
+	}
+}
