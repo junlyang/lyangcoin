0ff6875f021b265e11ecbc42046fe8f2333f2f0a
junlyang <eomlyang@naver.com>
2021-06-25 16:45:03 +0900

    애드 폼 개발중

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ import (
 	"github.com/junlyang/lyangcoin/blockchain"
 )
 
-const port string = ":4000"
+const (
+	port        string = ":4000"
+	templateDir string = "templates/"
+)
+
+var templates *template.Template
 
 type homeData struct {
 	PageTitle string
@@ -17,15 +22,34 @@ type homeData struct {
 }
 
 func home(rw http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/pages/home.gohtml"))
 	data := homeData{"안녕", blockchain.GetBlockchain().AllBlocks()}
-	tmpl.Execute(rw, data)
+	templates.ExecuteTemplate(rw, "home", data)
 }
+
+func add(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", nil)
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockchain().AddBlock(data)
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+
+	}
+	templates.ExecuteTemplate(rw, "add", nil)
+}
+
 func main() {
 	chain := blockchain.GetBlockchain()
 	chain.AddBlock("second block")
 
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+
 	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
+
 	fmt.Printf("Lisnening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
